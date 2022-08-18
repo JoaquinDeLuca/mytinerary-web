e583b3f9f1ffa352972eca256fb4f460b2b24efb
JoaquinDeLuca <joaqui222@gmail.com>
2022-08-18 20:22:27 -0300

    Se agregaron los componetes footer,welcome y sus style

---
+++ src/components/Footer.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import '../styles/Footer.css'
+
+export default function Footer() {
+  return (
+    <div className='Footer-container'>
+      <div className='Footer-web'>
+          <p className='Footer-icon'><i class="fa-brands fa-instagram"></i></p>
+          <p className='Footer-icon'><i class="fa-brands fa-facebook"></i></p>
+          <p className='Footer-icon'><i class="fa-brands fa-telegram"></i></p>
+      </div>
+      <div className='Footer-copyright'>
+        <p>  Mytinerary | © 2022</p>
+      </div>
+
+
+
+
+
+    </div>
+  )
+}
