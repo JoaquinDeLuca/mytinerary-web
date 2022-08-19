d2fb43987818e504377581a09d30638c918eeab6
JoaquinDeLuca <joaqui222@gmail.com>
2022-08-19 18:34:06 -0300

    se agregaron pages home,cities

---
+++ src/pages/Cities.js
@@ -0,0 +1,18 @@
+import WebsiteLayouts from "../layouts/WebsiteLayouts"
+
+export default function Cities() {
+  return (
+    <>
+        <WebsiteLayouts>
+            <h2>
+                Todas las cities podes ver aca  Main
+            </h2>
+        </WebsiteLayouts>
+    </>
+
+
+
+
+
+  )
+}
