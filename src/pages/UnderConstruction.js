4aff991e79a0a6645578608b2d3a54bc0bb4a762
Nico ojeda <nico_ojeda86@hotmail.com>
2022-08-21 19:35:58 -0300

    se crea pagina undercostruction

---
+++ src/pages/UnderConstruction.js
@@ -0,0 +1,12 @@
+import WebsiteLayouts from "../layouts/WebsiteLayouts"
+import "../styles/UnderConstruction.css"
+
+export default function UnderConstruction() {
+  return (
+    <WebsiteLayouts>
+    <div className="Under-container">
+      <img className="Under-image" src="https://ps.w.org/easy-under-construction/assets/banner-772x250.png?rev=2417171" alt="under"/>
+    </div>
+    </WebsiteLayouts>
+  )
+}
