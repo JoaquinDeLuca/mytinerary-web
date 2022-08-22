ec9ca7d6798d63da439667f135a40afdeb714ee1
LilAguh <aguezeochoa@gmail.com>
2022-08-22 10:38:47 -0300

    se cambia de ubicacion el componente Welcome

---
+++ src/components/Welcome.js
@@ -0,0 +1,11 @@
+import '../styles/Welcome.css'
+
+export default function Welcome() {
+  return (
+    <div className="Welcome-conatiner">
+      <h1 className="Welcome-title">My Tinerary</h1>
+
+      <p className='Welcome-slogan'>Find your perfect trip, designed by insiders who know and love their cities!</p>
+    </div>
+  )
+}
