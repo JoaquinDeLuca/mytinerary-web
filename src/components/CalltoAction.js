c5c0c66934d2e6679dbb149b68fd622f6c1248f2
JoaquinDeLuca <joaqui222@gmail.com>
2022-08-21 23:47:02 -0300

    Se agrego componente CalltoAction y su style

---
+++ src/components/CalltoAction.js
@@ -0,0 +1,12 @@
+import '../styles/CalltoAction.css'
+
+export default function CalltoAction() {
+    return (
+      
+        <div className='CalltoAction-container'>
+            <img className='CalltoAction-img' src='https://i.ibb.co/54jg3Ps/calltoaction.png'/>
+            <a className='CalltoAction-similbtn' href='#'>Start!</a>
+            <p className='CallltoAction-phrase'>Once a year travel to a place you've never been before</p>
+        </div>
+    )
+  }
