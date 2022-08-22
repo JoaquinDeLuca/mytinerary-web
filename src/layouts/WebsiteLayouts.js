f7b53a971be764597fa75c230f03443aa577059b
LilAguh <aguezeochoa@gmail.com>
2022-08-22 15:41:09 -0300

    se agregan classnames adicionales a layouts

--- src/layouts/WebsiteLayouts.js
+++ src/layouts/WebsiteLayouts.js
@@ -1,15 +1,16 @@
 import Footer from "../components/Footer"
+import '../styles/WebsiteLayouts.css'
 
 export default function WebsiteLayouts(props) {
 
   return (
     <>
-        <div>Navbar ira el componente</div>
-        <main>
-        { props.children}
-        </main>
-        <Footer/>
-    
+      <div>Navbar ira el componente</div>
+      <main>
+        {props.children}
+      </main>
+      <Footer />
+
     </>
   )
 }
