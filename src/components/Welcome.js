aec883add7517c102cd4744d26ad998449fea06a
Nico ojeda <nico_ojeda86@hotmail.com>
2022-08-23 13:55:13 -0300

    se ordena codigo

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,7 +4,6 @@ export default function Welcome() {
   return (
     <div className="Welcome-conatiner">
       <h1 className="Welcome-title">My Tinerary</h1>
-
       <p className='Welcome-slogan'>Find your perfect trip, designed by insiders who know and love their cities!</p>
     </div>
   )
