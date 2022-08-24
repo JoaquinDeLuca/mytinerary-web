6fc881747e786937828458c87b0d973445505e49
JoaquinDeLuca <joaqui222@gmail.com>
2022-08-23 21:47:42 -0300

    se agrego el componente scrollToTop

---
+++ src/components/ScrollToTop.js
@@ -0,0 +1,19 @@
+
+import { useLocation } from "react-router-dom"
+import { useEffect } from "react"
+
+export default function ScrollToTop() {
+
+    const { pathname } = useLocation()
+    // console.log(pathname)
+
+    useEffect(() => {
+
+        window.scrollTo(0, 0);
+
+    }, [pathname])
+
+  return (
+    <div>ScrollToTop</div>
+  )
+}
