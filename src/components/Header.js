abe649e4cd963997f45e77dd9099b0b9caba8bec
LilAguh <aguezeochoa@gmail.com>
2022-08-24 23:51:42 -0300

    se agrega el navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,48 +1,62 @@
-import {Link as LinkRouter} from 'react-router-dom'
+import { Link as LinkRouter } from 'react-router-dom'
 import '../styles/Header.css'
 import acceso from '../img/acceso.png';
 import logo3 from '../img/logo3.png';
-import {useState} from 'react'
+import lista from '../img/lista.png'
+import { useState } from 'react'
 
 const pages = [
-    {name: 'Home', to: '/'},
-    {name: 'Cities', to: '/cities'},
-    {name: 'New City', to: '/newcity'},
+    { name: 'Home', to: '/' },
+    { name: 'Cities', to: '/cities' },
+    { name: 'New City', to: '/newcity' },
 ]
 
-const link = (page)=><LinkRouter className='Header-link' to={page.to}>{page.name}</LinkRouter>
+const link = (page) => <li><LinkRouter className='Header-link' to={page.to}>{page.name}</LinkRouter></li>
 
 export default function Header() {
+    const [menu, setMenu] = useState(false)
+    const [open, setOpen] = useState(false)
 
-const [open, setOpen] = useState(false)
-
-const HandleOpen = ()=>{
-    if(open){
-        setOpen(false)
-    } else{
-        setOpen(true)
+    const toggleMenu = () => {
+        setMenu(!menu)
     }
-}
- 
-    return(
-        <header>
-            <img className='Header-logo' src={logo3} alt='logo'/>
-            <nav>
-                {pages.map(link)}
-            </nav>
-            <div>
-                <div className='Header-menu'>
-                {
-                    open 
-                        ? <ul>
-                            <li className='Header-li'>Sign up</li>
-                            <li className='Header-li'>Sign in</li>
-                        </ul> 
-                        : null 
-                }
+
+
+    const HandleOpen = () => {
+        if (open) {
+            setOpen(false)
+        } else {
+            setOpen(true)
+        }
+    }
+
+    return (
+        <header className={`${menu ? 'HeaderisActive' : ''} `}>
+            <img className='Header-logo' src={logo3} alt='logo' />
+            <div className='Header-NavContainer'>
+                <nav className={`Header-nav ${menu ? 'isActive' : ''} `}>
+                    <div className='Header-Backgound'>&nbsp;</div>
+                    <ul className='Header-LinksContainer'>
+                        {pages.map(link)}
+                    </ul>
+                </nav>
+                <div>
+                    <div className='Header-menu'>
+                        {
+                            open
+                                ? <ul className='Header-profileMenu'>
+                                    <li className='Header-li'>Sign up</li>
+                                    <li className='Header-li'>Sign in</li>
+                                </ul>
+                                : null
+                        }
+                    </div>
+                    <button className='Header-button' onClick={HandleOpen}><img className='Header-login' src={acceso} alt='acceso' /></button>
                 </div>
-                <button className='Header-button' onClick= {HandleOpen}><img className='Header-login' src={acceso} alt='acceso'/></button>
-            </div> 
+                <button className='Header-NavMenuButton' onClick={toggleMenu}>
+                    <img className='Header-NavMenuIcon' src={lista} alt='logo' />
+                </button>
+            </div>
         </header>
     )
 }
