1ca541f0e5fecc560fbdece1684f02f300ebbf0b
Nico ojeda <nico_ojeda86@hotmail.com>
2022-08-25 02:15:59 -0300

    se elimina codigo sin uso

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,6 +1,5 @@
 import CarouselCities from '../components/CarouselCities'
 import CalltoAction from '../components/CalltoAction'
-import WebsiteLayouts from "../layouts/WebsiteLayouts"
 import Welcome from '../components/Welcome'
 
 export default function Home() {
