d35d1615d981c8e6bd8fc6d1d6403f374e33838e
JoaquinDeLuca <joaqui222@gmail.com>
2022-08-25 01:49:40 -0300

    se agrega el un div para separar el h2

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -59,18 +59,22 @@ export default function Carousel(props) {
     // console.log("End: " + end)
 
     return (
-
-        <div className="Caorusel">
-            <Arrow icon={<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-caret-left" viewBox="0 0 16 16">
-            <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
-            </svg>} click={previous} />
-            <div className="Carousel-container">
-                {cities.slice(start, end).map(citiesView)}
+        <>
+            <div className="Carousel-h2">
+                <h2>Popular MYtineraries</h2>
             </div>
-            <Arrow icon={<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
-            <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
-            </svg>} click={next} />
-        </div>
+            <div className="Caorusel">
+                <Arrow icon={<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-caret-left" viewBox="0 0 16 16">
+                <path d="M10 12.796V3.204L4.519 8 10 12.796zm-.659.753-5.48-4.796a1 1 0 0 1 0-1.506l5.48-4.796A1 1 0 0 1 11 3.204v9.592a1 1 0 0 1-1.659.753z"/>
+                </svg>} click={previous} />
+                <div className="Carousel-container">
+                    {cities.slice(start, end).map(citiesView)}
+                </div>
+                <Arrow icon={<svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" fill="currentColor" class="bi bi-caret-right" viewBox="0 0 16 16">
+                <path d="M6 12.796V3.204L11.481 8 6 12.796zm.659.753 5.48-4.796a1 1 0 0 0 0-1.506L6.66 2.451C6.011 1.885 5 2.345 5 3.204v9.592a1 1 0 0 0 1.659.753z"/>
+                </svg>} click={next} />
+            </div>
+        </>
     )
 }
 
