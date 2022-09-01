9ef954ee392431727c4e71477e8906c71e90bcc8
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-01 08:28:11 -0300

    componente details

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,50 +1,74 @@
 import '../styles/Details.css'
 import axios from "axios"
 import { useEffect, useState } from 'react'
+import { useParams } from 'react-router-dom'
 
 export default function Details() {
 
   
   // Con el meteodo window... me traigo el id que pasa por URL
   let id = window.location.search
+  // let {id} = useParams()
+  // console.log(`Este es: ${id}`)
 
-  let idClean = id.slice(1)
+  // console.log(useParams())
+  // let idClean = id.slice(1)
   // console.log(idClean)
   
+
   const [cities, setCities] = useState([])
   
   useEffect(() => {
-    axios.get('http://localhost:4000/cities')
-      .then(response => setCities(response.data))
+    const obtenerDatos = () =>  {
+      axios.get(`http://localhost:4000/cities/${id}`)
+        .then(response => setCities(response.data))
+    }
+    obtenerDatos()
+  },[id])
 
-  },[])
-  let cityid = cities.find( city => city._id === idClean)
+  
+  // let cityid = cities.find( city => city._id === id)
   // console.log(cityid)
 
 
-  const printDetails = (cityid) => {
-    return(
-      <div className="Details-container">
-        <div className="Details-Card">
-          <div>
-            <img className="Details-img" src={cityid.photo} alt="img" />
-          </div>
-            <div className='Details-containe-p'>
-              <p className="Details-p">City:{cityid.city}</p>
-              <p className='Details-p'>Country:{cityid.country} </p>
-              <p className='Details-p'>population:{cityid.population} </p>
-              <p className='Details-p'>Fundation:{cityid.fundation} </p>
-            </div>
-        </div>
-      </div>
 
-    )
-  }
+
+  // const printDetails = (cityid) => {
+  //   return(
+  //     <div className="Details-container">
+  //       <div className="Details-Card">
+  //         <div>
+  //           <img className="Details-img" src={cityid.photo} alt="img" />
+  //         </div>
+  //           <div className='Details-containe-p'>
+  //             <p className="Details-p">City:{cityid.city}</p>
+  //             <p className='Details-p'>Country:{cityid.country} </p>
+  //             <p className='Details-p'>population:{cityid.population} </p>
+  //             <p className='Details-p'>Fundation:{cityid.fundation} </p>
+  //           </div>
+  //       </div>
+  //     </div>
+
+  //   )
+  // }
 
 
   return (
     <>
       {/* {printDetails(cityid)} */}
+      <div className="Details-container">
+        <div className="Details-Card">
+          <div>
+            <img className="Details-img" src={cities.photo} alt="img" />
+          </div>
+            <div className='Details-containe-p'>
+              <p className="Details-p">City:{cities.city}</p>
+              <p className='Details-p'>Country:{cities.country} </p>
+              <p className='Details-p'>population:{cities.population} </p>
+              <p className='Details-p'>Fundation:{cities.fundation} </p>
+            </div>
+        </div>
+      </div>
     </>
   )
 }
