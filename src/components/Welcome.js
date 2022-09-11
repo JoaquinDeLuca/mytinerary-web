fe96b038c2b384b8f5fa7473cbe7ae9fdad47f74
LilAguh <aguezeochoa@gmail.com>
2022-09-11 20:34:18 -0300

    se modifica estetica de welcome y se agrega efecto parallax en el fondo

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -1,14 +1,47 @@
 import '../styles/Welcome.css'
-import background from '../img/background.png';
+import agua from '../assets/parallax/agua.png'
+import fondo from '../assets/parallax/fondo.png'
+import frente from '../assets/parallax/frente.png'
+import nubes from '../assets/parallax/nubes.png'
+import sol from '../assets/parallax/sol.png'
+import { useState, useEffect } from 'react'
 
 export default function Welcome() {
+
+
+
+
+
+
+  const [offset, setOffset] = useState(0)
+
+  useEffect(() => {
+    function handleScroll() {
+      setOffset(window.pageYOffset)
+    }
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+
+
   return (
     <div className="Welcome-conatiner">
-      <img className='Welcome-background' src={background} alt='background' />
-      <div className='Welcome-text'>
-        <h1 className="Welcome-title">My Tinerary</h1>
+      <img className='Parallax-fondo' src={fondo} alt='' />
+      <img className='Parallax-sol' src={sol} alt='' style={{
+        transform: `translateY(${offset * 0.25}px)`,
+      }} />
+      <img className='Parallax-nubes' src={nubes} alt='' />
+      <img className='Parallax-agua' src={agua} alt='' />
+      <div className='Welcome-text' style={{ transform: `translateY(${offset * 0.3}px)`, overflow: `hidden`, }}>
+        <p className='Welcome-trips'>Trips</p>
+        <h1 className="Welcome-title">MyTinerary</h1>
         <p className='Welcome-slogan'>Find your perfect trip, designed by insiders who know and love their cities!</p>
       </div>
+      <img className='Parallax-frente' src={frente} alt='' />
     </div>
   )
 }
