5419bd7af42140b3b463b2a8df7b192fa8a45ecf
LilAguh <aguezeochoa@gmail.com>
2022-09-12 01:52:14 -0300

    se agrega la nueva imagen frontal al componente

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -4,6 +4,7 @@ import fondo from '../assets/parallax/fondo.png'
 import frente from '../assets/parallax/frente.png'
 import nubes from '../assets/parallax/nubes.png'
 import sol from '../assets/parallax/sol.png'
+import palmeras from '../assets/parallax/palmeras.png'
 import { useState, useEffect } from 'react'
 
 export default function Welcome() {
@@ -31,11 +32,10 @@ export default function Welcome() {
   return (
     <div className="Welcome-conatiner">
       <img className='Parallax-fondo' src={fondo} alt='' />
-      <img className='Parallax-sol' src={sol} alt='' style={{
-        transform: `translateY(${offset * 0.25}px)`,
-      }} />
+      <img className='Parallax-sol' src={sol} alt='' style={{ transform: `translateY(${offset * 0.13}px)`, }} />
       <img className='Parallax-nubes' src={nubes} alt='' />
       <img className='Parallax-agua' src={agua} alt='' />
+      <img className='Parallax-palmeras' src={palmeras} alt='' />
       <div className='Welcome-text' style={{ transform: `translateY(${offset * 0.3}px)`, overflow: `hidden`, }}>
         <p className='Welcome-trips'>Trips</p>
         <h1 className="Welcome-title">MyTinerary</h1>
