0379a52d6815c7b3864b8b83bd492bd6da3a84fb
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-14 18:27:39 -0300

    componente para iniciar session con google

---
+++ src/components/SingInGoogle.js
@@ -0,0 +1,52 @@
+import * as jose from 'jose'
+import { useEffect, useRef, useState } from 'react'
+import { usePostUserSingInMutation } from '../features/userApi'
+
+export default function SingInGoogle() {
+
+    const [user, setUser] = useState(0)
+
+    let [newUser] = usePostUserSingInMutation()
+
+    const buttonDiv = useRef(null)
+    // console.log(buttonDiv.current) 
+
+    async function handleCredentialResponse(response){
+       let userObject = jose.decodeJwt(response.credential)
+
+        const data = {
+            mail: userObject.email, 
+            password: userObject.sub, 
+            from:'google'
+        }
+
+        newUser(data)
+        // .then(response => console.log(response))
+        
+        setUser(localStorage.setItem('useriInfo', JSON.stringify(data)))
+        
+
+        window.location.replace('/')
+      }
+      // console.log(user)
+
+    useEffect(() =>{
+        /* global google */
+        google.accounts.id.initialize({
+            client_id: '858987294668-ujt6gjb9nmuhec61k7kavpnmva2lunt5.apps.googleusercontent.com',
+            callback: handleCredentialResponse,
+            context: 'signup'
+          });
+          google.accounts.id.renderButton(
+            buttonDiv.current,
+            { theme: "filled_blue", size: "mediun", text:'singup_with' , shape: "circle", type:"standard"}  // customization attributes
+          );
+    },[])
+
+
+  return (
+    <>
+        <div ref={buttonDiv}></div>
+    </>
+  )
+}
