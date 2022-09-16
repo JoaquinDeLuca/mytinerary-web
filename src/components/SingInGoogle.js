ed8d25144bc36aaf4a69e01d038baf295ab6637a
LilAguh <aguezeochoa@gmail.com>
2022-09-16 05:22:35 -0300

    se agregan alertas al signin de google

--- src/components/SingInGoogle.js
+++ src/components/SingInGoogle.js
@@ -1,52 +1,91 @@
 import * as jose from 'jose'
 import { useEffect, useRef, useState } from 'react'
 import { usePostUserSingInMutation } from '../features/userApi'
+import Alert from './alerts/Alert'
+import checkIcon from '../assets/icons/check.png'
+import errorIcon from '../assets/icons/exclamation.png'
 
 export default function SingInGoogle() {
 
-    const [user, setUser] = useState(0)
+  const [user, setUser] = useState(0)
 
+  const [list, setList] = useState([])
+  let toastProperties = null
 
-    let [newUser] = usePostUserSingInMutation()
-
-    const buttonDiv = useRef(null)
-    // console.log(buttonDiv.current) 
-
-    async function handleCredentialResponse(response){
-       let userObject = jose.decodeJwt(response.credential)
-
-        const data = {
-          mail: userObject.email, 
-          password: userObject.sub, 
-          from:'google',
-          id: userObject._id,
-          role: userObject.role
+  const showAlert = type => {
+    switch (type) {
+      case 'success':
+        toastProperties = {
+          id: list.length + 1,
+          title: 'Welcome',
+          description: 'Successful SignIn',
+          backgroundColor: '#5cb85c',
+          icon: checkIcon
         }
+        break;
+      case 'error':
+        toastProperties = {
+          id: list.length + 1,
+          title: 'Error',
+          description: 'Wrong email or password',
+          backgroundColor: '#d9534f',
+          icon: errorIcon
+        }
+        break;
+      default:
+        toastProperties = [];
+    }
+    setList([...list, toastProperties])
+  }
 
-        newUser(data)
-        .then(response =>  setUser(localStorage.setItem('useriInfo', JSON.stringify(response.data.response.user))))
-        
-        window.location.replace('/')
-      }
-      // console.log(user)
 
-    useEffect(() =>{
-        /* global google */
-        google.accounts.id.initialize({
-            client_id: '858987294668-ujt6gjb9nmuhec61k7kavpnmva2lunt5.apps.googleusercontent.com',
-            callback: handleCredentialResponse,
-            context: 'signup'
-          });
-          google.accounts.id.renderButton(
-            buttonDiv.current,
-            { theme: "filled_blue", size: "mediun", text:'singup_with' , shape: "circle", type:"standard"}  // customization attributes
-          );
-    },[])
+  let [newUser] = usePostUserSingInMutation()
+
+  const buttonDiv = useRef(null)
+
+  async function handleCredentialResponse(response) {
+    let userObject = jose.decodeJwt(response.credential)
+
+    const data = {
+      mail: userObject.email,
+      password: userObject.sub,
+      from: 'google',
+      id: userObject._id,
+      role: userObject.role
+    }
+
+    newUser(data)
+      .then(response => {
+        if (response.data.success === true) {
+          showAlert('success')
+          setUser(localStorage.setItem('useriInfo', JSON.stringify(response.data.response.user)))
+          window.location.replace('/')
+        }
+        else {
+          showAlert('error')
+        }
+      })
+  }
+
+
+  useEffect(() => {
+    /* global google */
+    google.accounts.id.initialize({
+      client_id: '858987294668-ujt6gjb9nmuhec61k7kavpnmva2lunt5.apps.googleusercontent.com',
+      callback: handleCredentialResponse,
+      context: 'signup'
+    });
+    google.accounts.id.renderButton(
+      buttonDiv.current,
+      { theme: "filled_blue", size: "mediun", text: 'singup_with', shape: "circle", type: "standard" }  // customization attributes
+    );
+  }, [])
 
 
   return (
     <>
-        <div ref={buttonDiv}></div>
+      <Alert toastlist={list} setList={setList} />
+      <div ref={buttonDiv}></div>
     </>
   )
 }
