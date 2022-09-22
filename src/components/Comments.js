28e77210d4d33e50e2f68c5fbddf1863143d4f83
LilAguh <aguezeochoa@gmail.com>
2022-09-22 00:28:56 -0300

    se modifica el componente comments

--- src/components/Comments.js
+++ src/components/Comments.js
@@ -1,6 +1,7 @@
 import '../styles/Comments.css'
 import axios from "axios"
 import { useEffect, useState } from 'react'
+import TextEditor from './TextEditor'
 
 
 export default function Comments(props) {
@@ -8,7 +9,7 @@ export default function Comments(props) {
     const id = props.data._id
     const [comments, setComments] = useState([])
 
-    const [open,setOpen] = useState(false)
+    const [open, setOpen] = useState(false)
 
     const HandleOpen = () => {
         if (open) {
@@ -19,44 +20,67 @@ export default function Comments(props) {
     }
 
 
-    useEffect(()=>{
+    useEffect(() => {
         axios.get(`http://localhost:4000/comments/itinerary/${id}`)
-        .then(response => setComments(response.data.response))
-    },[id])
+            .then(response => setComments(response.data.response))
+    }, [id])
 
-// console.log(comments)
+    // console.log(comments)
 
 
-const showComments = (comment)=>{
-  return (
-    <div className='Comment-container'>
-        <div className='Comment-user'>
-            <div className='Comment-containerimg'>
-                <img className='Comment-img' src={comment.user.photo} />
+    const showComments = (comment) => {
+        return (
+            <div className='Comment-container'>
+                <div className='Comment-user'>
+                    <div className='Comment-containerimg'>
+                        <img className='Comment-img' src={comment.user.photo} />
+                    </div>
+                    <p>{comment.user.name}</p>
+                </div>
+                <div className='Comment-p'>
+                    <p>{comment.comment}</p>
+                </div>
             </div>
-            <p>{comment.user.name}</p>
-        </div>
-        <div className='Comment-p'>
-            <p>{comment.comment}</p>
-        </div>
-    </div>
 
-  )
-}
+        )
+    }
 
+    const [isBold, setBold] = useState(false)
+    const [message, setMessage] = useState("")
 
+    const handleClick = () => {
+        setBold((prevValue) => {
+            return !prevValue
+        })
+    }
 
-return(
+    const handleChange = event => {
+        setMessage(event.target.value)
+        if (isBold) {
+            console.log("*" + message + "*")
+        }
+        else {
+            console.log(message)
+        }
+    }
+
+    return (
 
         <div className='Comment-full'>
             <h3 className='Comment-h3' onClick={HandleOpen} >Comments <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-            <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z"/>
-</svg></h3>
+                <path d="M7.247 11.14 2.451 5.658C1.885 5.013 2.345 4 3.204 4h9.592a1 1 0 0 1 .753 1.659l-4.796 5.48a1 1 0 0 1-1.506 0z" />
+            </svg></h3>
             <div className="Comment-Container2">
                 {
-                open
-                    ?  comments.map(showComments) :null
+                    open
+                        ? comments.map(showComments) : null
                 }
+                <div>
+                    {/* <textarea className="Comment-textarea" value={message} onChange={handleChange} name="content" />
+                    <button onClick={handleClick}>Bold</button> */}
+
+                    <TextEditor />
+                </div>
             </div>
         </div>
     )
