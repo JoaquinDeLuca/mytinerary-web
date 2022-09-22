b0de338bc369d72e73427d442e58ec2b4fe49f57
JoaquinDeLuca <joaqui222@gmail.com>
2022-09-22 18:06:28 -0300

    se agrego titulo a la pagina y clg

--- src/pages/NewItinerary.js
+++ src/pages/NewItinerary.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import { useParams } from 'react-router-dom'
 import '../styles/NewItinerary.css'
 
 export default function NewItinerary() {
+  let {id} = useParams()
+  console.log(id)
+
+
+
   return (
     <div>
       <h1>New Itinerary</h1>
