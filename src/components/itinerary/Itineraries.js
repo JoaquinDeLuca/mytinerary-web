4270323ae5410db25e0c5920598aa40174180162
JoaquinDeLuca <joaqui222@gmail.com>
2022-11-09 16:58:44 -0300

    se agrega un loading spinner

--- src/components/itinerary/Itineraries.js
+++ src/components/itinerary/Itineraries.js
@@ -1,14 +1,32 @@
 import '../../styles/Itinerary.css'
 import Itinerary from './itineraries/Itinerary'
 import { useGetItinerariesQuery } from '../../features/itinerariesApi'
-
+import Button from 'react-bootstrap/Button';
+import Spinner from 'react-bootstrap/Spinner';
 
 export default function Itineraries(props) {
 
     const id = props.data._id
     const name = props.data.city
 
-    const { data: itineraries } = useGetItinerariesQuery(id)
+    const { data: itineraries, isLoading } = useGetItinerariesQuery(id)
+
+    if(isLoading){
+        return(
+          <div className="vh-100 d-flex justify-content-center align-items-center">
+            <Button variant="dark" disabled>
+              <Spinner
+                as="span"
+                animation="grow"
+                size="sm"
+                role="status"
+                aria-hidden="true"
+              />
+              Loading...
+            </Button>
+          </div>
+        )
+    }
 
     return (
         <div className="Itinerary">
