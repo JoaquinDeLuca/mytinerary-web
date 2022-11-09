7ebb3d9a623dda0e3b7af8e5d5679a176533ded7
JoaquinDeLuca <joaqui222@gmail.com>
2022-11-09 17:14:04 -0300

    se agregan las actividades y los comentarios de los usuarios

--- src/components/itinerary/itineraries/Itinerary.js
+++ src/components/itinerary/itineraries/Itinerary.js
@@ -12,9 +12,17 @@ export default function Itinerary(props) {
         <div className="Itinerary">
             <div className='ItineraryLine'></div>
             <h3 className="ItineraryTitle">{itinerary.name}</h3>
+            <div className="itineraryLikeAndUser">
+                <img className="itineraryImgUser" src={itinerary.user.photo} />
+                <p className="itineraryUserName"> By {itinerary.user.name}</p>
+                <Like itinerary={itinerary} />
+            </div>
             <div className="Itinerary-ActivitiesContainer">
-                <h3 className="Itinerary-ActivityTitle">Activities:</h3>
+                <h3 className="Itinerary-ActivityTitle m-0 p-0">Activities</h3>
                 <Activities data={itinerary} />
+            </div>
+            <div className='itineraryContainerComments'>
+                <Comments data={itinerary}/>
             </div>
         </div>
     )
