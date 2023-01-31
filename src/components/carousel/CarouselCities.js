c65281a41f4fea066484b097e59679ebdd65d98c
JoaquinDeLuca <joaqui222@gmail.com>
2023-01-31 17:32:23 -0300

    added spinner loading

--- src/components/carousel/CarouselCities.js
+++ src/components/carousel/CarouselCities.js
@@ -7,9 +7,16 @@ export default function CarouselCities() {
   const initialCity = 0
   const endCity = 12
 
-  const { data: cities } = useGetAllCitiesQuery([])
+  const { data: cities, isLoading } = useGetAllCitiesQuery([])
 
-
+  if(isLoading){
+    return(
+      <div className="Cities-Loader">
+        <span className="loader"></span>
+      </div>
+    )
+  }
+  
   return (
     <div>
       <Carousel data={cities?.slice(initialCity, endCity)} range={4} interval={5} slides={3} />
